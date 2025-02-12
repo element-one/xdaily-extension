c197ca3e83decee4a3322962e7952d1430906d25
nebulium <yuhan_may@126.com>
2025-02-12 13:56:29 +0800

    feat: add login page

--- sidepanel/index.tsx
+++ sidepanel/index.tsx
@@ -1,124 +1,16 @@
-import { useMemo, useState, type ReactNode } from "react"
+import { useState } from "react"
 
 import "../styles/global.css"
 
-import { Bookmark, FolderOpenDot, Search, Settings } from "lucide-react"
-
-import { AiSuggestionPanel } from "./components/AiSuggestionPanel"
-import { BoardPanel } from "./components/BoardPanel"
-import { MeNavbarItem } from "./components/MeNavbarItem"
-import { SearchPanel } from "./components/SearchPanel"
-import { SettingPanel } from "./components/SettingPanel"
-
-enum NavbarItemKey {
-  SEARCH = "search",
-  SUGGESTION = "ai suggestions",
-  BOARD = "board",
-  SETTING = "setting"
-}
-
-type NavbarItem = {
-  key: NavbarItemKey
-  icon: ReactNode
-  tooltip: string
-  component: ReactNode
-}
-
-const NavbarItems: NavbarItem[] = [
-  {
-    key: NavbarItemKey.SEARCH,
-    icon: <Search className="w-5 h-5" />,
-    tooltip: "Search",
-    component: <SearchPanel />
-  },
-  {
-    key: NavbarItemKey.SUGGESTION,
-    icon: <Bookmark className="w-5 h-5" />,
-    tooltip: "AI Suggestions",
-    component: <AiSuggestionPanel />
-  },
-  {
-    key: NavbarItemKey.BOARD,
-    icon: <FolderOpenDot className="w-5 h-5" />,
-    tooltip: "Board",
-    component: <BoardPanel />
-  }
-] as const
-
-const SettingNavbarItem: NavbarItem = {
-  key: NavbarItemKey.SETTING,
-  icon: <Settings className="w-5 h-5" />,
-  tooltip: "Setting",
-  component: <SettingPanel />
-}
+import { DashboardPage } from "./components/DashboardPage"
+import { LoginPage } from "./components/LoginPage"
 
 const IndexSidePanel = () => {
-  const [navbarItemKey, setNavbarItemKey] = useState<NavbarItemKey>(
-    NavbarItemKey.SEARCH
-  )
-
-  chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === "sidepanel_close_self") {
-      window.close()
-    }
-  })
-
-  const currentNavbarItem = useMemo(() => {
-    if (navbarItemKey) {
-      const AllNavbarItems = NavbarItems.concat(SettingNavbarItem)
-      return AllNavbarItems.find((item) => item.key === navbarItemKey)
-    }
-    return undefined
-  }, [navbarItemKey])
-
-  const toggleDrawer = (itemKey: NavbarItemKey) => {
-    setNavbarItemKey(itemKey)
-  }
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
 
   return (
-    // TODO fonts
     <div className="flex flex-row w-full h-screen overflow-hidden bg-muted">
-      {/* main */}
-      <div className="flex flex-col relative p-1 transition-all w-[calc(100%-50px)]">
-        <div className="bg-white w-full relative flex-1 overflow-hidden rounded-xl">
-          <div className="p-4 h-screen">
-            <div className="flex flex-col h-full overflow-y-auto hide-scrollbar">
-              {currentNavbarItem?.component}
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* aside */}
-      <div className="max-w-[50px] py-2 flex flex-col items-center gap-4 h-full w-[50px]">
-        <aside className="flex h-full flex-col">
-          {/* top tabs */}
-          <div className="flex-grow flex items-center overflow-y-auto gap-4 flex-col">
-            {NavbarItems.map((item) => {
-              return (
-                <MeNavbarItem
-                  key={item.key}
-                  handleClick={() => toggleDrawer(item.key)}
-                  isTargeted={item.key === currentNavbarItem.key}
-                  content={item.icon}
-                  tooltip={item.tooltip}
-                />
-              )
-            })}
-          </div>
-          {/* bottom buttons */}
-          <div className="flex flex-col gap-4">
-            {/* Settings */}
-            <MeNavbarItem
-              key={SettingNavbarItem.key}
-              handleClick={() => toggleDrawer(SettingNavbarItem.key)}
-              isTargeted={SettingNavbarItem.key === currentNavbarItem.key}
-              content={SettingNavbarItem.icon}
-              tooltip={SettingNavbarItem.tooltip}
-            />
-            <div className="w-8 h-8 bg-primary-brand rounded-full"></div>
-          </div>
-        </aside>
-      </div>
+      {isAuthenticated ? <DashboardPage /> : <LoginPage />}
     </div>
   )
 }
