330cc4f450c2eb924f76471cc1cf59a7e2604d04
nebulium <yuhan_may@126.com>
2025-04-10 15:40:51 +0800

    feat: change layout, add tab menu for post page

--- sidepanel/components/panels/PostPanel/TweetListItem.tsx
+++ sidepanel/components/panels/PostPanel/TweetListItem.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs"
 import { EyeIcon } from "lucide-react"
 import { type FC } from "react"
 
@@ -17,11 +18,18 @@ export const TweetListItem: FC<TweetListProps> = (props) => {
       <div className="flex items-center gap-3 max-w-[80%]">
         <div className="flex flex-col">
           <div className="text-sm flex items-center gap-x-1">
-            <div className="font-semibold">@{props.screenName}</div>
+            <div className="font-semibold text-primary-brand">
+              @{props.screenName}
+            </div>
           </div>
           <div className="text-xs text-muted-foreground line-clamp-4 px-1">
             {props.content}
           </div>
+          {props.postedAt && (
+            <div className="px-1 text-xs mt-1">
+              {dayjs(props.postedAt).format("h:mmA · YYYY/M/D")}
+            </div>
+          )}
         </div>
       </div>
       <div className="flex flex-row gap-3 items-center">
