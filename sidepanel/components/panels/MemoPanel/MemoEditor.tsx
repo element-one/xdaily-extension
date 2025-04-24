0110adfbb0cf439b2d05dae6d0153b277dfc19f7
nebulium <yuhan_may@126.com>
2025-04-24 13:37:52 +0800

    chore: patch-package to fix problems of plasmo esm

--- sidepanel/components/panels/MemoPanel/MemoEditor.tsx
+++ sidepanel/components/panels/MemoPanel/MemoEditor.tsx
@@ -1,13 +1,24 @@
 import { useEffect, type FC } from "react"
 
+import type { MemoItem } from "~types/memo"
+
 import "@blocknote/core/fonts/inter.css"
+
+import { BlockNoteView } from "@blocknote/mantine"
+
 import "@blocknote/mantine/style.css"
 
-import type { MemoItem } from "~types/memo"
+import { useCreateBlockNote } from "@blocknote/react"
 
 interface MemoEditorProps {
   memo: MemoItem
 }
 export const MemoEditor: FC<MemoEditorProps> = ({ memo }) => {
-  return <div>{memo.id}</div>
+  const editor = useCreateBlockNote()
+  return (
+    <div className="w-full flex-1 min-h-0 overflow-y-auto stylized-scroll">
+      {memo.id}
+      <BlockNoteView editor={editor} />
+    </div>
+  )
 }
