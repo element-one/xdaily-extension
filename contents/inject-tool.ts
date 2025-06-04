64dda1133f31cd90d0128d6b93bea4103c030d8c
nebulium <yuhan_may@126.com>
2025-06-04 14:27:34 +0800

    feat: use a custom element name

--- contents/inject-tool.ts
+++ contents/inject-tool.ts
@@ -12,25 +12,12 @@ export const config: PlasmoCSConfig = {
 const COLLECT_BUTTON_CONT = "xdaily-collect-button-cont"
 const loadingTweets = new Set<string>()
 
-const getCollectIcon = () => `
-  <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" >
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M2.6665 4.66732C2.6665 2.82637 4.15889 1.33398 5.99984 1.33398H9.9998C11.8407 1.33398 13.3331 2.82637 13.3331 4.66732V12.6601C13.3331 14.3626 11.3416 15.2866 10.0416 14.1872L8.43033 12.8245C8.18181 12.6143 7.81786 12.6143 7.56934 12.8245L5.958 14.1872C4.65806 15.2866 2.6665 14.3626 2.6665 12.6601V4.66732ZM5.99984 2.66732C4.89527 2.66732 3.99984 3.56275 3.99984 4.66732V12.6601C3.99984 13.2276 4.66369 13.5356 5.097 13.1692L6.70834 11.8064C7.4539 11.1759 8.54578 11.1759 9.29133 11.8064L10.9026 13.1692C11.3359 13.5356 11.9998 13.2276 11.9998 12.6601V4.66732C11.9998 3.56275 11.1044 2.66732 9.9998 2.66732H5.99984Z" fill="#34C759"></path>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4.66602C8.36819 4.66602 8.66667 4.96449 8.66667 5.33268V5.99935H9.33333C9.70152 5.99935 10 6.29783 10 6.66602C10 7.03421 9.70152 7.33268 9.33333 7.33268H8.66667V7.99935C8.66667 8.36754 8.36819 8.66602 8 8.66602C7.63181 8.66602 7.33333 8.36754 7.33333 7.99935V7.33268H6.66667C6.29848 7.33268 6 7.03421 6 6.66602C6 6.29783 6.29848 5.99935 6.66667 5.99935H7.33333V5.33268C7.33333 4.96449 7.63181 4.66602 8 4.66602Z" fill="#34C759"></path>
-    </svg>
-`
-
-const getSpinnerIcon = () => `
-  <svg viewBox="0 0 24 24" fill="none" width="16" height="16">
-    <circle cx="12" cy="12" r="10" stroke="#FFE600" stroke-width="4" stroke-linecap="round" />
-  </svg>
-`
-
 // twitter is SPA
 const observeTweets = () => {
   const observer = new MutationObserver(() => {
     document.querySelectorAll("article").forEach((tweet) => {
       const hasCollectButton = tweet.querySelector(`.${COLLECT_BUTTON_CONT}`)
-      const bookmarkButton = findTweetButton("bookmark", tweet)
+      const bookmarkButton = findTweetButton("bookmark", tweet, false)
       if (!hasCollectButton && bookmarkButton) {
         injectButton(tweet, bookmarkButton)
       }
@@ -40,7 +27,7 @@ const observeTweets = () => {
 }
 
 const createCollectButton = (tweet: HTMLElement) => {
-  const host = document.createElement("div")
+  const host = document.createElement("xdaily-collect-button")
   host.className = COLLECT_BUTTON_CONT
   const shadow = host.attachShadow({ mode: "open" })
 
