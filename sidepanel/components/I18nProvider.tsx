5a404de5a7ce6c3c8d658c7da755f701e4320302
nebulium <yuhan_may@126.com>
2025-07-07 12:53:16 +0800

    feat: get and update lang with api info

--- sidepanel/components/I18nProvider.tsx
+++ sidepanel/components/I18nProvider.tsx
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, type ReactNode } from "react"
 
 import i18n, { initI18n } from "../../locales/i18n"
 import { Loading } from "./ui/Loading"
 
-export function I18nProvider({ children }) {
+export function I18nProvider({
+  children,
+  defaultLang
+}: {
+  children: ReactNode
+  defaultLang?: string
+}) {
   const [ready, setReady] = useState(i18n.isInitialized)
 
   useEffect(() => {
     if (!i18n.isInitialized) {
-      initI18n().then(() => setReady(true))
+      initI18n(defaultLang).then(() => setReady(true))
     } else {
       setReady(true)
     }
