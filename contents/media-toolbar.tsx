41b7145fd4b6cb0cffca98dde7b6e019a8e171a2
nebulium <yuhan_may@126.com>
2025-08-11 11:35:38 +0800

    feat: deal special situation inside twitter video

--- contents/media-toolbar.tsx
+++ contents/media-toolbar.tsx
@@ -1,13 +1,12 @@
 import clsx from "clsx"
 import cssText from "data-text:~/styles/global.css"
-import { BotIcon, LoaderCircleIcon } from "lucide-react"
+import { BotIcon } from "lucide-react"
 import type { PlasmoCSConfig } from "plasmo"
 import React, { useEffect, useRef, useState, type FC } from "react"
 
 import { sendToBackground } from "@plasmohq/messaging"
 
 import { getMediaInfoFromEl } from "~libs/media"
-import { extractTweetDataFromTweet, getUserIdFromTweet } from "~libs/tweet"
 
 export const config: PlasmoCSConfig = {
   // TODO currently only show in these two sites
@@ -21,96 +20,6 @@ export const getStyle = () => {
   return style
 }
 
-interface TooltipProps {
-  children: React.ReactNode
-  content: string
-  side?: "top" | "bottom" | "left" | "right"
-  sideOffset?: number
-  className?: string
-}
-
-const Tooltip: React.FC<TooltipProps> = ({
-  children,
-  content,
-  side = "top",
-  sideOffset = 16,
-  className
-}) => {
-  const offsetStyle: Record<string, React.CSSProperties> = {
-    top: {
-      bottom: `100%`,
-      marginBottom: sideOffset,
-      left: "50%",
-      transform: "translateX(-50%)"
-    },
-    bottom: {
-      top: "100%",
-      marginTop: sideOffset,
-      left: "50%",
-      transform: "translateX(-50%"
-    },
-    left: {
-      right: "100%",
-      marginRight: sideOffset,
-      top: "50%",
-      transform: "translateY(-50%"
-    },
-
-    right: {
-      left: "100%",
-      marginLeft: sideOffset,
-      top: "50%",
-      transform: "translateY(-50%"
-    }
-  }
-
-  return (
-    <div className={clsx("relative group", className)}>
-      {children}
-      <div
-        className={clsx(
-          "absolute z-50 whitespace-nowrap bg-fill-bg-light border border-fill-bg-input rounded-lg px-3 py-2.5 text-text-default-regular text-xs opacity-0 group-hover:opacity-100 transition pointer-events-none"
-        )}
-        style={offsetStyle[side]}>
-        {content}
-      </div>
-    </div>
-  )
-}
-
-interface ToolbarButtonProps {
-  onClick?: () => void
-  isLoading: boolean
-  icon: React.ReactNode
-  tooltip: string
-}
-const ToolbarButton: FC<ToolbarButtonProps> = ({
-  onClick,
-  isLoading,
-  icon,
-  tooltip
-}) => {
-  const handleClick = () => {
-    onClick?.()
-  }
-  return (
-    <Tooltip content={tooltip} side="left" sideOffset={8}>
-      <div
-        onClick={handleClick}
-        className={clsx(
-          "font-geist flex justify-center items-center w-8 h-8 rounded cursor-pointer hover:bg-fill-bg-deep",
-          isLoading ? "cursor-not-allowed" : "cursor-pointer"
-        )}>
-        {isLoading ? (
-          <LoaderCircleIcon className="size-8 animate-spin text-primary-brand" />
-        ) : (
-          <>{icon}</>
-        )}
-      </div>
-    </Tooltip>
-  )
-}
-
 const Toolbar = () => {
   const [isVisible, setIsVisible] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
@@ -119,9 +28,18 @@ const Toolbar = () => {
 
   useEffect(() => {
     const observer = new MutationObserver(() => {
-      document.querySelectorAll("img, video").forEach((media) => {
-        media.addEventListener("mouseover", () => showToolbar(media))
-        media.addEventListener("mouseleave", hideToolbar)
+      document.querySelectorAll("img, video").forEach((media: HTMLElement) => {
+        if ((media as HTMLElement).dataset.bound === "true") return
+        media.dataset.bound = "true"
+        let targetElement = media
+        if (media.tagName.toLowerCase() === "video") {
+          const container = media.closest('[data-testid="videoComponent"]')
+          if (container) {
+            targetElement = container as HTMLElement
+          }
+        }
+        targetElement.addEventListener("mouseover", () => showToolbar(media))
+        targetElement.addEventListener("mouseleave", hideToolbar)
       })
     })
 
